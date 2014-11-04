5e65b96da2f8fee6ffa33ff91f44cb653cc70d68
Art Kuts <bcdkey@gmail.com>
2014-11-04 13:30:42 +0300

    added Procfile, changed static rules

--- PizzaMaster/wsgi.py
+++ PizzaMaster/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PizzaMaster.settings")
-
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PizzaMaster.settings")
+application = Cling(get_wsgi_application())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PizzaMaster.wsgi --log-file -
